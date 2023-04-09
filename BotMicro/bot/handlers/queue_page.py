ec49197248894ac58eefa9bfc5496b832c344fca
mamsdeveloper <butvin.mihail@yandex.ru>
2023-04-09 07:17:58 +0300

    base queue page

--- BotMicro/bot/handlers/queue_page.py
+++ BotMicro/bot/handlers/queue_page.py
@@ -18,7 +18,46 @@ async def queue_page_open_handler(query: CallbackQuery, message: Message, callba
         )
         return
 
+    if queue.creator == message.chat.id:
+        await show_queue_for_creator(queue, message)
+    else:
+        await show_queue_for_member(queue, message)
+
+
+async def show_queue_for_creator(queue: Queue, message: Message):
+    text = f'Queue: {queue.name}\nEnroll key: <code>{queue.key}</code>\n\n'
+    text += build_queue_text(queue) 
     await message.edit_text(
-        text=f'Queue: {queue.name}',
+        text=text,
         reply_markup=OpenMenuKeyboard()
     )
+
+
+async def show_queue_for_member(queue: Queue, message: Message):
+    text = f'Queue: {queue.name}\n\n'
+    text += build_queue_text(queue) 
+    await message.edit_text(
+        text=text,
+        reply_markup=OpenMenuKeyboard()
+    )
+
+
+def build_queue_text(queue: Queue):
+    size = len(queue.members)
+
+    text = ''
+    for pos, member_data in enumerate(queue.members):
+        if pos - queue.cursor < 0:
+            tick = '⚫'
+        elif pos - queue.cursor == 0:
+            tick = '🔘'
+        elif pos - queue.cursor < size // 3:
+            tick = '🟢'
+        elif pos - queue.cursor < size // 3 * 2:
+            tick = '🟡'
+        else:
+            tick = '🔴'
+        
+        text += f'{tick} {member_data[1]}\n'
+    
+    return text
